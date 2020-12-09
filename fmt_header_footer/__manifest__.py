7d09ebd746e8bde099e419a28c900260fec3b86a
Eng Ahmed Sherby <ahmadsherby@gmail.com>
2020-12-09 12:13:29 +0300

    FMT report
    invoice print

---
+++ fmt_header_footer/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Faisal Header Footer",
+    'summary': """
+        Faisal Header Footer""",
+    'description': """
+        Faisal Header Footer
+    """,
+    'author': "Eng. Ahmed Sherby, helcon",
+    'website': "https://telenoc.org",
+    'category': 'Account',
+    'version': '0.1',
+    'depends': ['web'],
+    'data': [
+        'report/invoice_report.xml',
+    ],
+}
